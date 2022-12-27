3c4632575572f4fca689d403bf3250c7eb827e2d
stcoderts <115059135+stcoderts@users.noreply.github.com>
2022-12-27 08:10:39 +0500

    just

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -14,7 +14,7 @@ public class PrimeNumbers {
         if (num<1){
             return false;
         }else {
-            for (int i=2;i<Math.sqrt(num);i++){
+            for (int i=2;i<=Math.sqrt(num);i++){
                 if (num%i==0) return false;
             }
             return true;
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,14 +1,14 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
-    public static void buildPhrase(char... chars) {
+    public void buildPhrase(char... chars) {
         String word = "";
         int a=0;
         while(a<chars.length){
             word+=chars[a];
             a++;
         }
-        System.out.println(word);
+        System.out.print(word);
     }
 
 }
